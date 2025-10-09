41af565ae328fea5b91afccb1205e7ee8262a3e9
Razorblade3456 <abraham1leyva@gmail.com>
2025-10-08 23:19:58 -0700

    added google auth 2

--- src/config/googleClient.ts
+++ src/config/googleClient.ts
@@ -1,9 +1,2 @@
-/**
- * Provide your Google OAuth client ID here if you don’t want to rely on the
- * Vite environment variable (VITE_GOOGLE_CLIENT_ID).
- *
- * Example:
- * export const GOOGLE_CLIENT_ID = '1234567890-your-client-id.apps.googleusercontent.com';
- */
-export const GOOGLE_CLIENT_ID = '';
+export const GOOGLE_CLIENT_ID = '79775733699-m2t6l70fngeo69s4qjo5pmnoqo8enccm.apps.googleusercontent.com';
 

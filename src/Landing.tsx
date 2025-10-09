b09ec242663ade5d6ba510cb3b8b7ef6a51714f7
Razorblade3456 <abraham1leyva@gmail.com>
2025-10-08 20:33:50 -0700

    installed decoder

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,4 +1,5 @@
 import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
 export function Landing() {
   return (
@@ -6,6 +7,7 @@ export function Landing() {
         <GoogleLogin
             onSuccess={credentialResponse => {
                 console.log(credentialResponse);
+                console
             }}
             onError={() => {
                 console.log('Login Failed');

59b95a6900fd4dd20f242cab08a15bc87283ff29
Razorblade3456 <abraham1leyva@gmail.com>
2025-10-08 20:38:07 -0700

    a

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -2,12 +2,16 @@ import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 
 export function Landing() {
+
+  const navigate = useNavigate()
+
   return (
     <>   
         <GoogleLogin
             onSuccess={credentialResponse => {
                 console.log(credentialResponse);
                 console.log(jwtDecode(credentialResponse.credential));
+                navigate('/home')
             }}
             onError={() => {
                 console.log('Login Failed');
